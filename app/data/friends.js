a61c371844080c754c4225dd8843fb5f378aaaea
ab1228 <alexb8734@gmail.com>
2019-07-15 23:32:58 -0500

    add friends array to friends.js and modified survey html

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [
+
+    {
+        name: "Frank Sosa",
+        photo: "http://dismagazine.com/uploads/2011/08/notw_silhouette-1.jpg",
+        score: ["5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"]
+    }
+
+]
